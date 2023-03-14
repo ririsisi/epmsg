3fcf554f3084cea1a616a9928099b888ff092b3e
ririsisi <QQnn2791996@>
2023-03-14 09:42:08 +0800

    01-搭建SpringBoot环境

---
+++ epmsg-web/src/main/java/com/epsoft/msg/EpmsgApplication.java
@@ -0,0 +1,11 @@
+package com.epsoft.msg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpmsgApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EpmsgApplication.class,args);
+    }
+}
