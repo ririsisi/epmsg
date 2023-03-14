e73ae493f82410b905cc1cab7eea70ae62b7cd89
ririsisi <QQnn2791996@>
2023-03-14 10:47:58 +0800

    03-引入Logback日志

--- epmsg-web/src/main/java/com/epsoft/msg/web/TestController.java
+++ epmsg-web/src/main/java/com/epsoft/msg/web/TestController.java
@@ -1,12 +1,16 @@
 package com.epsoft.msg.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class TestController {
     @RequestMapping("/test")
     private String test() {
+        System.out.println("sout:测试sout");
+        log.info("log:测试log");
         return "hello world";
     }
 
